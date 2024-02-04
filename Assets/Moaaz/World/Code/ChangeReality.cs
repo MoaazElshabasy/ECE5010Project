688f2e617aa0b4f3c1bd33b141d23ed90a9015fc
Moaaz M Elshabasy <mmmybelshaba@mun.ca>
2024-02-04 01:33:45 -0330

    Change Reality Phase 1

---
+++ Assets/Moaaz/World/Code/ChangeReality.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkenSpriteOnKeyPress : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private bool isChasmReality = false;
+    private Color originalColor;
+    private Color darkenedColor = new Color(0.2f, 0.2f, 0.2f);
+
+    void Start()
+    {
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            isChasmReality = !isChasmReality;
+
+            if (isChasmReality)
+            {
+                spriteRenderer.color = darkenedColor;
+            }
+            else
+            {
+                spriteRenderer.color = originalColor;
+            }
+        }
+    }
+}
+
---
+++ Assets/Moaaz/World/Code/DisableEnableObject.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableEnableObject : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Collider2D[] colliders;
+    public bool isEnabled = true; 
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        colliders = GetComponents<Collider2D>();
+        spriteRenderer.enabled = isEnabled;
+        foreach (Collider2D collider in colliders)
+        {
+            collider.enabled = isEnabled;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isEnabled = !isEnabled;
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.enabled = isEnabled;
+            }
+
+            if (colliders != null)
+            {
+                foreach (Collider2D collider in colliders)
+                {
+                    collider.enabled = isEnabled;
+                }
+            }
+        }
+    }
+}
+
+
+
+
